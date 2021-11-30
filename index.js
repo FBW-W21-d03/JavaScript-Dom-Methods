86d7deef44456e0e1bce37077d13cb0cad951666
sprotte <metatron1972@live.de>
2021-11-30 09:42:14 +0100

    sprotte

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hallo Welt");
+document.getElementById("paragraph").innerHTML = "wilkommen!";
+function myFunction(){
+console.log("Ich wurden gecklickt");  
+document.getElementById("paragrph").innerHTML = "Wilkommen! schön!"; + name;
+}
+  
