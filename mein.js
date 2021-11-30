2d70d198354096c1f7b2fb64f553f69c90b538ae
kawanaza <kawanbabani@gmail.com>
2021-11-30 09:49:13 +0100

    Done

---
+++ mein.js
@@ -0,0 +1,9 @@
+
+
+
+function losGet(){ 
+document.getElementById("Platzhalter").innerHTML= 100*1.16;
+} 
+
+document.getElementById("btn").style.background="gray";
+document.getElementById("btn").style.fontSize="35px";
